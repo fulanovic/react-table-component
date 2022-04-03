634dab88f1e05c366df5add22e8a873894584eba
Bojan Fulanovic <fulanovic@gmail.com>
2022-04-03 21:56:10 +0200

    search debounce added

--- src/table/assets/search.jsx
+++ src/table/assets/search.jsx
@@ -2,42 +2,57 @@ import React from 'react';
 import Input from './input';
 
 const SearchTerm = ({
-    searchTerm,
-    setSearchTerm,
-    data,
-    filteredData,
-    setCurrentPage,
-    noSearchDataCondition,
-    searchNoDataItemPrefix,
-    label = 'Search',
-    placeholder = 'Start typing search term'
+   searchTerm,
+   setSearchTerm,
+   debounceDelay = 1000,
+   data,
+   filteredData,
+   setCurrentPage,
+   noSearchDataCondition,
+   searchNoDataItemPrefix,
+   label = 'Search',
+   placeholder = 'Start typing search term'
 }) => {
-    return (
-        <div className='search-term'>
-            <Input
-                label={`${label} ${
-                    searchTerm.length !== 0
-                        ? filteredData.length > 0
-                            ? `- ${filteredData.length}/${data.length}`
-                            : ''
-                        : ''
-                }`}
-                name='search'
-                onChange={({ target: input }) => {
-                    const searchTerm = input.value;
-                    setCurrentPage(1);
-                    setSearchTerm(searchTerm);
-                }}
-                placeholder={placeholder}
-            />
-            {noSearchDataCondition && (
-                <div className='mb-5'>
-                    {`${searchNoDataItemPrefix} `}
-                    <span style={{ fontWeight: 700 }}>{`${searchTerm}`}</span>
-                </div>
-            )}
-        </div>
-    );
+   const debounce = (callback, delay = debounceDelay) => {
+      let timeout;
+      return (...args) => {
+         clearTimeout(timeout);
+         timeout = setTimeout(() => {
+            callback(...args);
+         }, delay);
+      };
+   };
+
+   const updateStateDebounce = debounce((searchTerm) => {
+      setCurrentPage(1);
+      setSearchTerm(searchTerm);
+   });
+
+   const handleOnChange = ({ target: input }) =>
+      updateStateDebounce(input.value);
+
+   return (
+      <div className='search-term'>
+         <Input
+            label={`${label} ${
+               searchTerm.length !== 0
+                  ? filteredData.length > 0
+                     ? `- ${filteredData.length}/${data.length}`
+                     : ''
+                  : ''
+            }`}
+            name='search'
+            onChange={handleOnChange}
+            placeholder={placeholder}
+         />
+         {noSearchDataCondition && (
+            <div className='mb-5'>
+               {`${searchNoDataItemPrefix} `}
+               <span style={{ fontWeight: 700 }}>{`${searchTerm}`}</span>
+            </div>
+         )}
+      </div>
+   );
 };
 
 export default SearchTerm;
